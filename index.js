c5a122558570ce9aa66581ede40764553030f0e6
gangadhar <141738423+dudduvarigangadhar@users.noreply.github.com>
2024-02-25 15:28:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
+// Write your code here.
 import {Component} from 'react'
 import './index.css'
 
 class LettersCalculator extends Component {
+  state = {counter: 0}
+
+  countChars = event => {
+    this.setState({counter: event.target.value.length})
+  }
+
   render() {
+    const {counter} = this.state
+
     return (
       <div className="bg-container">
         <div className="text-field">
-          <h1 className="">Calculate the Letters you enter</h1>
-          <p>Enter the phrase</p>
-          <input type="text" placeholder="Enter the phrase" />
-          <>No.of letters: 0</>
+          <h1 className="heading">Calculate the Letters you enter</h1>
+          <label htmlFor="label-name" className="input-heading">
+            Enter the phrase
+          </label>
+          <input
+            type="text"
+            placeholder="Enter the phrase"
+            id="label-name"
+            className="input-field"
+            onChange={this.countChars}
+          />
+          <div className="counter-box">
+            <p className="counter-part">No.of letters: {counter}</p>
+          </div>
         </div>
         <div>
           <img
